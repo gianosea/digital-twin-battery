6ad403a1d350750a4e28409367821268531e9ad1
Nicholas Sandy K <cindynikita6@gmail.com>
2026-05-29 04:56:52 +0700

    fix missing Link import di analytic

--- src/app/analytic/page.js
+++ src/app/analytic/page.js
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 // Import komponen grafik dari Recharts
 import {
   LineChart, Line, AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
